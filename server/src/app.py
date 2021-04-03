f143a8f8c2d59d00f785de3a19b53dc686c82ca4
captain price <danielroemo99@gmail.com>
2021-04-03 10:44:42 +0000

    from server: created a working dockerfile

--- server/src/app.py
+++ server/src/app.py
@@ -9,6 +9,5 @@ def index():
 def users():
     return 'Users!'
 
-    
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1999)
